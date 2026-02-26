42d91f254be4b4e4b1ee9c02e111792d8c3f5253
Dasha <dunm3rd@yandex.ru>
2026-02-26 14:44:46 +0300

    animeshki

---
+++ neko.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import ttk
+import requests
+from io import BytesIO
+from PIL import Image, ImageTk
+
+def get_random_neko():
+    json_response = requests.get("https://nekos.best/api/v2/neko")
+    json_response.raise_for_status()
+    data = json_response.json()
+
+    image_url = data['results'][0]['url']
+    
+    img_response = requests.get(image_url, stream=True)
+    img_response.raise_for_status()
+    
+    img_bytes = BytesIO(img_response.content)
+    pil_image = Image.open(img_bytes)
+    pil_image.thumbnail((500, 500), Image.Resampling.LANCZOS)
+    
+    return ImageTk.PhotoImage(pil_image)
+
+def update_image():
+    new_img = get_random_neko()
+    if new_img:
+        label.config(image=new_img)
+        label.image = new_img
+
+root = tk.Tk()
+root.title("Генератор аниме девочек")
+root.geometry("600x650")
+
+label = ttk.Label(root)
+label.pack(pady=10, expand=True)
+
+btn = ttk.Button(root, text="Получить новую картинку :3", command=update_image)
+btn.pack(pady=20)
+
+update_image()
+
+root.mainloop()
